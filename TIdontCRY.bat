4a348f8c40f7d44cce7e0808ed6ffe6aead0f1d6
Snehasish Nayak <sn3hasishnayak@gmail.com>
2017-05-24 13:35:29 +0530

    Add files via upload

---
+++ TIdontCRY.bat
@@ -0,0 +1,195 @@
+cd "\directory\"
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\WanaCrypt0r /f
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /va /f
+TIdontCRY.exe
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /va /f
+SC DELETE mssecsvc2.0
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /va /f
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\WanaCrypt0r /f
+del /s /q /f c:\taskhsvc.exe
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /va /f
+del /s /q /f c:\taskhsvc.exe 
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /va /f
+del /s /q /f c:\taskche.exe
+del /s /q /f d:\taskche.exe
+
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\WanaCrypt0r /f
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /va /f
+del /s /q /f c:\@WanaDecryptor@.exe
+del /s /q /f c:\@WanaDecryptor@.bmp
+del /s /q /f c:\@Please_Read_Me@.txt
+del /s /q /f d:\@WanaDecryptor@.exe
+del /s /q /f e:\@WanaDecryptor@.exe
+del /s /q /f f:\@WanaDecryptor@.exe
+del /s /q /f g:\@WanaDecryptor@.exe
+del /s /q /f h:\@WanaDecryptor@.exe
+del /s /q /f i:\@WanaDecryptor@.exe
+del /s /q /f j:\@WanaDecryptor@.exe
+del /s /q /f k:\@WanaDecryptor@.exe
+del /s /q /f l:\@WanaDecryptor@.exe
+del /s /q /f m:\@WanaDecryptor@.exe
+del /s /q /f n:\@WanaDecryptor@.exe
+del /s /q /f o:\@WanaDecryptor@.exe
+del /s /q /f f:\@WanaDecryptor@.exe
+del /s /q /f p:\@WanaDecryptor@.exe
+del /s /q /f q:\@WanaDecryptor@.exe
+del /s /q /f r:\@WanaDecryptor@.exe
+del /s /q /f s:\@WanaDecryptor@.exe
+del /s /q /f t:\@WanaDecryptor@.exe
+del /s /q /f u:\@WanaDecryptor@.exe
+del /s /q /f v:\@WanaDecryptor@.exe
+del /s /q /f w:\@WanaDecryptor@.exe
+del /s /q /f x:\@WanaDecryptor@.exe
+del /s /q /f y:\@WanaDecryptor@.exe
+del /s /q /f z:\@WanaDecryptor@.exe
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\WanaCrypt0r /f
+del /s /q /f c:\taskse.exe
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\WanaCrypt0r /f
+del /s /q /f c:\wcry.exe
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\WanaCrypt0r /f
+del /s /q /f c:\taskdl.exe
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /va /f
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\WanaCrypt0r /f
+del /s /q /f c:\taskkill.exe
+del /s /q /f d:\taskkill.exe
+del /s /q /f e:\taskkill.exe
+del /s /q /f f:\taskkill.exe
+del /s /q /f g:\taskkill.exe
+del /s /q /f h:\taskkill.exe
+del /s /q /f i:\taskkill.exe
+del /s /q /f j:\taskkill.exe
+del /s /q /f k:\taskkill.exe
+del /s /q /f l:\taskkill.exe
+del /s /q /f m:\taskkill.exe
+del /s /q /f n:\taskkill.exe
+del /s /q /f o:\taskkill.exe
+del /s /q /f f:\taskkill.exe
+del /s /q /f p:\taskkill.exe
+del /s /q /f q:\taskkill.exe
+del /s /q /f r:\taskkill.exe
+del /s /q /f s:\taskkill.exe
+del /s /q /f t:\taskkill.exe
+del /s /q /f u:\taskkill.exe
+del /s /q /f v:\taskkill.exe
+del /s /q /f w:\taskkill.exe
+del /s /q /f x:\taskkill.exe
+del /s /q /f y:\taskkill.exe
+del /s /q /f z:\taskkill.exe
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\WanaCrypt0r /f
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /va /f
+rd /s /q %temp%
+SC DELETE mssecsvc2.0
+SC DELETE @WanaDecryptor@.exe
+SC DELETE mssecsvc2.0
+SC DELETE taskdl.exe
+SC DELETE taskse.exe
+SC DELETE wannacry.exe
+SC DELETE tasksche.exe
+SC DELETE wcry.exe
+SC DELETE 111.exe
+SC DELETE lhdfrgui.exe
+
+cd "\directory\"
+copy abc.bmp C:\
+@echo off
+reg add "hkcu\control panel\desktop" /v wallpaper /t REG_SZ /d "" /f 
+reg add "hkcu\control panel\desktop" /v wallpaper /t REG_SZ /d "C:\abc.bmp" /f 
+reg delete "hkcu\Software\Microsoft\Internet Explorer\Desktop\General" /v WallpaperStyle /f
+reg add "hkcu\control panel\desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters 
+exits
+REG DELETE HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /va /f
+del /s /q /f g:\taskkill.exe
+del /s /q /f h:\taskkill.exe
+del /s /q /f i:\taskkill.exe
+del /s /q /f j:\taskkill.exe
+del /s /q /f k:\taskkill.exe
+del /s /q /f l:\taskkill.exe
+del /s /q /f m:\taskkill.exe
+del /s /q /f n:\taskkill.exe
+del /s /q /f o:\taskkill.exe
+del /s /q /f f:\taskkill.exe
+del /s /q /f p:\taskkill.exe
+del /s /q /f q:\taskkill.exe
+del /s /q /f r:\taskkill.exe
+del /s /q /f s:\taskkill.exe
+del /s /q /f t:\taskkill.exe
+del /s /q /f u:\taskkill.exe
+del /s /q /f v:\taskkill.exe
+del /s /q /f w:\taskkill.exe
+del /s /q /f x:\taskkill.exe
+del /s /q /f y:\taskkill.exe
+del /s /q /f z:\taskkill.exe
+del /s /q /f c:\*.WNCRY
+del /s /q /f d:\*.WNCRY
+del /s /q /f e:\*.WNCRY
+del /s /q /f f:\*.WNCRY
+del /s /q /f g:\*.WNCRY
+del /s /q /f h:\*.WNCRY
+del /s /q /f i:\*.WNCRY
+del /s /q /f j:\*.WNCRY
+del /s /q /f k:\*.WNCRY
+del /s /q /f l:\*.WNCRY
+del /s /q /f m:\*.WNCRY
+del /s /q /f n:\*.WNCRY
+del /s /q /f o:\*.WNCRY
+del /s /q /f f:\*.WNCRY
+del /s /q /f p:\*.WNCRY
+del /s /q /f q:\*.WNCRY
+del /s /q /f r:\*.WNCRY
+del /s /q /f s:\*.WNCRY
+del /s /q /f t:\*.WNCRY
+del /s /q /f u:\*.WNCRY
+del /s /q /f v:\*.WNCRY
+del /s /q /f w:\*.WNCRY
+del /s /q /f x:\*.WNCRY
+del /s /q /f y:\*.WNCRY
+del /s /q /f z:\*.WNCRY
+del /s /q /f c:\taskhsvc.exe
+del /s /q /f d:\taskhsvc.exe
+del /s /q /f e:\taskhsvc.exe
+del /s /q /f f:\taskhsvc.exe
+del /s /q /f g:\taskhsvc.exe
+del /s /q /f h:\taskhsvc.exe
+del /s /q /f i:\taskhsvc.exe
+del /s /q /f j:\taskhsvc.exe
+del /s /q /f k:\taskhsvc.exe
+del /s /q /f l:\taskhsvc.exe
+del /s /q /f m:\taskhsvc.exe
+del /s /q /f n:\taskhsvc.exe
+del /s /q /f o:\taskhsvc.exe
+del /s /q /f f:\taskhsvc.exe
+del /s /q /f p:\taskhsvc.exe
+del /s /q /f q:\taskhsvc.exe
+del /s /q /f r:\taskhsvc.exe
+del /s /q /f s:\taskhsvc.exe
+del /s /q /f t:\taskhsvc.exe
+del /s /q /f u:\taskhsvc.exe
+del /s /q /f v:\taskhsvc.exe
+del /s /q /f w:\taskhsvc.exe
+del /s /q /f x:\taskhsvc.exe
+del /s /q /f y:\taskhsvc.exe
+del /s /q /f z:\taskhsvc.exe
+del /s /q /f e:\taskche.exe
+del /s /q /f f:\taskche.exe
+del /s /q /f g:\taskche.exe
+del /s /q /f h:\taskche.exe
+del /s /q /f i:\taskche.exe
+del /s /q /f j:\taskche.exe
+del /s /q /f k:\taskche.exe
+del /s /q /f l:\taskche.exe
+del /s /q /f m:\taskche.exe
+del /s /q /f n:\taskche.exe
+del /s /q /f o:\taskche.exe
+del /s /q /f f:\taskche.exe
+del /s /q /f p:\taskche.exe
+del /s /q /f q:\taskche.exe
+del /s /q /f r:\taskche.exe
+del /s /q /f s:\taskche.exe
+del /s /q /f t:\taskche.exe
+del /s /q /f u:\taskche.exe
+del /s /q /f v:\taskche.exe
+del /s /q /f w:\taskche.exe
+del /s /q /f x:\taskche.exe
+del /s /q /f y:\taskche.exe
+del /s /q /f z:\taskche.exe
